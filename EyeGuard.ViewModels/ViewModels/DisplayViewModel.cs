901736600f212b4d0ce6cc220b4aaa4fdc1f55ac
omartoaba <42046875+omartoaba@users.noreply.github.com>
2023-07-23 17:22:58 +0100

    adding viewModels classes

---
+++ EyeGuard.ViewModels/ViewModels/DisplayViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyeGuard.ViewModels.ViewModels
+{
+    internal class DisplayView
+    {
+    }
+}
