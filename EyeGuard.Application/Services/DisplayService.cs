752279f6268dae3004b428f19e2f00e0a3daeefa
omartoaba <42046875+omartoaba@users.noreply.github.com>
2023-07-23 17:25:38 +0100

    adding display service

---
+++ EyeGuard.Application/Services/DisplayService.cs
@@ -0,0 +1,13 @@
+﻿using EyeGuard.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyeGuard.Application
+{
+    public class DisplayService : IDisplayService
+    {
+    }
+}
